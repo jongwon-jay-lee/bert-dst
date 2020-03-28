ba927637ef2affb23ea6c8e121a51b666b3fbd05
Jongwon Lee <jay722.lee@samsung.com>
2020-03-28 22:26:26 +0900

    update gitignore

--- train.sh
+++ train.sh
@@ -3,11 +3,12 @@
 # TASK can be "dstc2_clean", "woz2", "sim-m", or "sim-r"
 TASK="dstc2_clean"
 # Directory for dstc2-clean, woz_2.0, sim-M, or sim-R, which contains json files
-DATA_DIR=/path/to/dstc2-clean
+# DATA_DIR=/path/to/dstc2-clean
+DATA_DIR=./dstc2
 # Directory of the pre-trained [BERT-Base, Uncased] model
-PRETRAINED_BERT=/path/to/uncased_L-12_H-768_A-12
+PRETRAINED_BERT=./bert_model/uncased_L-12_H-768_A-12
 # Output directory of trained checkpoints
-OUTPUT_DIR=/path/to/output
+OUTPUT_DIR=./output
 
 mkdir -p $OUTPUT_DIR
 python main.py \
